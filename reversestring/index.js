e3a5963f758547a4e03d60efedc8458dd08e915c
keerthanak-tw <keerthana.kasthuril@gmail.com>
2022-04-25 22:08:46 +0530

    Reverse string solution

--- reversestring/index.js
+++ reversestring/index.js
@@ -6,6 +6,34 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+// My solution
+function reverse(str) {
+    return str.split('').reverse().join('');
+}
 
 module.exports = reverse;
+
+// Course soution 1
+// The built-in function reverse helps to reverse an array
+// Hence, convert the string into array by splitting
+// Use built in function
+// Join the array element using join function
+function reverseOne (str) {
+    return str.split('').reverse().join('');
+}
+
+// Course solution 2
+function reverseTwo (str) {
+    let reversed = '';
+
+    for (let char of str) {
+        reversed = char + reversed;
+    }
+
+    return reversed;
+}
+
+// Course solution 3
+function reverseThree (str) {
+    return str.split('').reduce((reversed, char) =>  char + reversed, '');
+}
