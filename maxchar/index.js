ebe080381b161be79c0cfb4375de7ef2f0d4de4c
keerthanak-tw <keerthana.kasthuril@gmail.com>
2022-04-28 11:22:48 +0530

    Max char solution

--- maxchar/index.js
+++ maxchar/index.js
@@ -5,6 +5,37 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+// function maxChar(str) {
+//     const countMap = {};
+//     let max = 0;
+//     let maxChar = '';
+
+//     for (let char of str) {
+//         countMap[char] = countMap[char] + 1 || 1;
+//     }
+//     for (let char in countMap) {
+//         if (countMap[char] > max) {
+//             max = countMap[char];
+//             maxChar = char;
+//         }
+//     }
+//     return maxChar;
+// }
 
 module.exports = maxChar;
+
+
+// Another solution
+function maxChar(str) {
+    const max = { count: 0 };
+    str.split('').reduce((countMap, current) => {
+        countMap[current] = countMap[current] + 1 || 1;
+        if (countMap[current] > max.count) {
+            max.count = countMap[current];
+            max.char = current;
+        }
+        return countMap;
+    }, {});
+
+    return max.char;
+}
