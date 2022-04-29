739de0dbdd09bbf021119be68c7df818e66244c4
keerthanak-tw <keerthana.kasthuril@gmail.com>
2022-04-29 11:31:02 +0530

    Fizzbuzz solution

--- fizzbuzz/index.js
+++ fizzbuzz/index.js
@@ -12,6 +12,33 @@
 //   4
 //   buzz
 
-function fizzBuzz(n) {}
+function fizzBuzz(n) {
+    for (let i = 1; i <= n; ++i) {
+        let logger = '';
+        if ( i % 3 === 0) {
+            logger += 'fizz';
+        }
+        if (i % 5 === 0) {
+            logger += 'buzz';
+        }
+        console.log(logger !== '' ? logger : i);
+    }
+}
 
 module.exports = fizzBuzz;
+
+
+// Course solution
+function fizzBuzzOne(n) {
+    for (let i = 1; i <= n; ++i) {
+         if (i % 3 === 0 && i % 5 === 0) {
+             console.log('fizzbuzz');
+         } else if (i % 3 === 0) {
+             console.log('fizz');
+         } else if (i % 5 === 0) {
+            console.log('buzz');
+        } else {
+            console.log(i);
+        }
+    }
+}
