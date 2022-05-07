71e3d3cf717b740b5182a74f02c13c1d2982da4e
keerthanak-tw <keerthana.kasthuril@gmail.com>
2022-05-07 23:56:32 +0530

    Anagrams solution

--- anagrams/index.js
+++ anagrams/index.js
@@ -8,6 +8,52 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+function getCountMap(str) {
+    return str.split('').reduce((countMap, current) => {
+        countMap[current] = countMap[current] + 1 || 1;
+        return countMap;
+    }, {});
+}
+
+
+function anagrams(stringA, stringB) {
+    const countMapA = getCountMap(stringA.replace(/[^\w]/g, "").toLowerCase());
+    const countMapB = getCountMap(stringB.replace(/[^\w]/g, "").toLowerCase());
+    if (Object.keys(countMapA).length === Object.keys(countMapB).length &&
+        Object.keys(countMapA).every((letter) => countMapA[letter] === countMapB[letter])) {
+        return true;
+    }
+    return false;
+}
 
 module.exports = anagrams;
+
+// Course solutions
+
+function buildCharMap(str) {
+    const charMap = {};
+    for (let char of str.replace(/[^\w]/g, "").toLowerCase()) {
+        charMap[char] = charMap[char] + 1 || 1;
+    }
+    return charMap;
+}
+function anagramsOne(stringA, stringB) {
+    const aCharMap = buildCharMap(stringA);
+    const bCharMap = buildCharMap(stringB);
+    if (Object.keys(aCharMap).length !== Object.keys(bCharMap).length) {
+        return false;
+    }
+    for (let char in aCharMap) {
+        if (aCharMap[char] !== bCharMap[char]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+function cleanString(str) {
+    return str.replace(/[^\w]/g, "").toLowerCase().split('').sort().join('');
+}
+function anagramsWithHack(stringA, stringB) {
+    return cleanString(stringA) === cleanString(stringB);
+}
