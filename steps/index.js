6725cb7126229bcdf00d93d1ef44131cbc5fe13b
keerthanak-tw <keerthana.kasthuril@gmail.com>
2022-05-08 22:55:15 +0530

    Steps solution

--- steps/index.js
+++ steps/index.js
@@ -17,6 +17,31 @@
 //       '### '
 //       '####'
 
-function steps(n) {}
+function steps(n) {
+    for (let row = 0; row < n; ++row) {
+        let string = '';
+        for (let column = 0; column < n; ++column) {
+            if (column <= row) {
+                string += '#';
+            } else {
+                string += ' ';
+            }
+        }
+        console.log(string);
+    }
+}
 
 module.exports = steps;
+
+
+function stepsWithRecursion(n, row = 0, stair = '') {
+    if (n === row) {
+        return;
+    }
+    if (n === stair.length) {
+        console.log(stair);
+        return stepsWithRecursion(n, row + 1);
+    }
+    stair += stair.length <= row ? '#' : ' ';
+    return stepsWithRecursion(n, row, stair);
+}
