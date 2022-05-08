4a32164c1f588f89e635ce776729c9a04b53241a
keerthanak-tw <keerthana.kasthuril@gmail.com>
2022-05-08 22:13:31 +0530

    Capitalize solution

--- capitalize/index.js
+++ capitalize/index.js
@@ -7,6 +7,34 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-function capitalize(str) {}
+function capitalize(str) {
+    return str.split(' ').map((word) => {
+        return word[0].toUpperCase() + word.slice(1);
+    }).join(' ');
+}
 
 module.exports = capitalize;
+
+function capitalizeOne(str) {
+    const words = [];
+
+    for(let word of str.split(' ')) {
+       words.push(word[0].toUpperCase() + word.slice(1));
+    }
+
+    return words.join(' ');
+}
+
+function capitalizeTwo(str) {
+    let result = str[0].toUpperCase();
+
+    for (let i = 1; i < str.length; ++i) {
+        if (str[i - 1] === ' ') {
+            result += str[i].toUpperCase();
+        } else {
+            result += str[i];
+        }
+    }
+
+    return result;
+}
