69e14fc76cb320d91499150cd899d9f4823fea33
keerthanak-tw <keerthana.kasthuril@gmail.com>
2022-05-26 19:48:51 +0530

    Queue from stack solution

--- qfroms/index.js
+++ qfroms/index.js
@@ -14,6 +14,37 @@
 
 const Stack = require('./stack');
 
-class Queue {}
+class Queue {
+    constructor() {
+        this.first = new Stack();
+        this.second = new Stack();
+    }
+
+    add(record) {
+        this.first.push(record);
+    }
+
+    remove() {
+        while(this.first.peek()) {
+            this.second.push(this.first.pop());
+        }
+        const recordToRemove = this.second.pop();
+        while(this.second.peek()) {
+            this.first.push(this.second.pop());
+        }
+        return recordToRemove;
+    }
+
+    peek() {
+        while(this.first.peek()) {
+            this.second.push(this.first.pop());
+        }
+        const peekRecord = this.second.peek();
+        while(this.second.peek()) {
+            this.first.push(this.second.pop());
+        }
+        return peekRecord;
+    }
+}
 
 module.exports = Queue;
