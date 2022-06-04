2c11f038ac97668a5830f081984a24c7a8843ba3
keerthanak-tw <keerthana.kasthuril@gmail.com>
2022-06-04 14:01:37 +0530

    Tree implementation

--- tree/index.js
+++ tree/index.js
@@ -10,8 +10,47 @@
 // on the tree class.  Each method should accept a
 // function that gets called with each element in the tree
 
-class Node {}
+class Node {
+    constructor(data) {
+        this.data = data;
+        this.children = [];
+    }
 
-class Tree {}
+    add(data) {
+        this.children.push(new Node(data));
+    }
+
+    remove(data) {
+        this.children = this.children.filter((child) => child.data !== data);
+    }
+}
+
+class Tree {
+    constructor() {
+        this.root = null;
+    }
+
+    traverseBF(cb) {
+        const nodes = [this.root];
+
+        while(nodes.length) {
+            const nodeToProcess = nodes.shift();
+            nodes.push(...nodeToProcess.children);
+            
+            cb(nodeToProcess);
+        }
+    }
+
+    traverseDF(cb) {
+        const nodes = [this.root];
+
+        while(nodes.length) {
+            const nodeToProcess = nodes.shift();
+            nodes.unshift(...nodeToProcess.children);
+            
+            cb(nodeToProcess);
+        }
+    }
+}
 
 module.exports = { Tree, Node };
