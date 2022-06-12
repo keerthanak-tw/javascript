d2153944aebe9860c3c8f2c021de96b1729b9030
keerthanak-tw <keerthana.kasthuril@gmail.com>
2022-06-12 22:15:53 +0530

    Events handler

--- events/index.js
+++ events/index.js
@@ -4,16 +4,25 @@
 // have methods 'on', 'trigger', and 'off'.
 
 class Events {
+  constructor() {
+    this.events = {};
+  }
   // Register an event handler
-  on(eventName, callback) {}
+  on(eventName, callback) {
+    this.events[eventName] = this.events[eventName] ? [...this.events[eventName], callback] : [callback];
+  }
 
   // Trigger all callbacks associated
   // with a given eventName
-  trigger(eventName) {}
+  trigger(eventName) {
+    if (this.events[eventName]) { this.events[eventName].forEach((fn) => fn()); }
+  }
 
   // Remove all event handlers associated
   // with the given eventName
-  off(eventName) {}
+  off(eventName) {
+    delete this.events[eventName];
+  }
 }
 
 module.exports = Events;
